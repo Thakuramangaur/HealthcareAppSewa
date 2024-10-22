4487c9c41e38fc58552a9734040afc1014d1223b
Aman gaur <129651493+Thakuramangaur@users.noreply.github.com>
2024-10-22 18:54:45 +0530

    Add files via upload

---
+++ images.tsx
@@ -0,0 +1,7 @@
+export const images = {
+    logo: require('./Image/plusButton.png'),
+    family: require('./Image/Sewa1.png'),
+    family1: require('./Image/family1.png'),
+    family2: require('./Image/family2.png'),
+    family3: require('./Image/family3.png'),
+  };
